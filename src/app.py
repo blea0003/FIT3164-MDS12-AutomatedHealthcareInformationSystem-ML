0da9cbeb04aa8876888b0ade0e09f6502a7fddd6
Bryan Lean <blea0003@student.monash.edu>
2023-10-02 21:41:32 +0800

    Added CORS for SOP

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, CORS, request, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import cv2
 import numpy as np
 from main import patient_registration_parser
